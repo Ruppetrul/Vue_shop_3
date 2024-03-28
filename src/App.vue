7daf2a49aef546f0875455a02e95ba231502ed69
serg <nautbek@gmail.com>
2024-03-28 20:05:51 +0300

    Fix background color.

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,17 @@
 </script>
 
 <template>
-  <div id="app">
-    <div class="container">
-      <div class="container-fluid-lg">
-        <router-view></router-view>
-      </div>
+  <div class="container">
+    <div class="container-fluid-lg">
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <style>
+  html {
+    background-color: aliceblue;
+  }
   #app {
     height: 100%;
     padding-top: 10px;
