a9c161315f840c02d9c0c909339cd713bbb2fd0a
serg <nautbek@gmail.com>
2024-03-30 13:07:05 +0300

    Change RouterLink to router-link.

--- src/components/main/Item.vue
+++ src/components/main/Item.vue
@@ -27,12 +27,12 @@
   <div class="item_cart_container">
     <div class="item_cart">
       <div class="item_avatar">
-        <RouterLink :to="`/detail/${props.itemData.id}`">
+        <router-link :to="`/detail/${props.itemData.id}`">
           <img :src="props.itemData.avatar_url" width="200" height="200" alt="Дефолтная картинка" loading="lazy">
-        </RouterLink>
+        </router-link>
       </div>
       <div class="item_detail">
-        <RouterLink :to="'/detail/' + props.itemData.id">{{ props.itemData.title }}</RouterLink>
+        <router-link :to="'/detail/' + props.itemData.id">{{ props.itemData.title }}</router-link>
         <h5>{{ props.itemData.price }}</h5>
       </div>
       <div class="item_footer">
