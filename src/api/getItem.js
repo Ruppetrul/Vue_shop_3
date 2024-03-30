c736cb14b16a4e8fcded50af41592baf84d9e2f3
serg <nautbek@gmail.com>
2024-03-30 13:01:03 +0300

    Add error message to getItem function.

--- src/api/getItem.js
+++ src/api/getItem.js
@@ -5,7 +5,7 @@ export async function getItem(item_id) {
         const { data } = await axios.get(`/api/ajax/product/${item_id}`);
         return data;
     } catch (error) {
-        console.error(error);
+        console.error('Error getItem:', error);
         throw error;
     }
 }
