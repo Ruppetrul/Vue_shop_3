85c353184a8889a77ff4cc54e28633005969d6b7
serg <nautbek@gmail.com>
2024-03-30 12:57:56 +0300

    Add interpolation to getItem function.

--- src/api/getItem.js
+++ src/api/getItem.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getItem(item_id) {
     try {
-        const response = await axios.get('/api/ajax/product/' + item_id);
+        const response = await axios.get(`/api/ajax/product/${item_id}`);
         return response.data;
     } catch (error) {
         console.error(error);
