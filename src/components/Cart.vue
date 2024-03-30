21e3eddda053fcb6112e47044a3ad85d5bbb5a80
serg <nautbek@gmail.com>
2024-03-30 12:51:01 +0300

    Add preloader to Cart route.

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,8 +1,20 @@
 <script setup>
+  import { ref } from "vue";
+  import ItemPreloader from "@/components/cart/ItemPreloader.vue";
+  const isLoading = ref(true);
 </script>
 
 <template>
-  Cart page
+  <h1>Корзина</h1>
+  <hr>
+  <template v-if="isLoading">
+    <div class="placeholder_image">
+      <ItemPreloader v-for="index in 10" :key="index" />
+    </div>
+  </template>
+  <template v-else>
+
+  </template>
 </template>
 
 <style scoped>
